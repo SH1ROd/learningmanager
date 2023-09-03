6361fb3321285ea6f3fd68bb824a9b597122b3e0
Dexter.L <863444090@qq.com>
2023-09-03 11:18:11 +0800

    Changes

---
+++ src/test/java/com/learningmanager/LearningmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
